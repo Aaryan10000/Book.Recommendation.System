60c0b6513a022e556b7b7a056472f4d3bf0e2b90
Aaryan Dawalkar <aaru.dawalkar@gmail.com>
2025-10-28 16:12:58 +0530

    Added method to download large model File

---
+++ downloadModel.py
@@ -0,0 +1,17 @@
+import os
+import gdown
+
+
+def download_from_drive(file_id, output_path):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    print(f"📥 Downloading from Google Drive...\nURL: {url}")
+    gdown.download(url, output_path, quiet=False)
+    print(f"✅ Download complete! Saved as {output_path}")
+
+
+if __name__ == "__main__":
+    file_id = "1lcOZ5tcGYr9UMf9Eg0pYJpFkJxhXLwFJ"
+    output_path = os.path.join("recommender_Model", "similarity_matrices.pkl")
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    download_from_drive(file_id, output_path)
