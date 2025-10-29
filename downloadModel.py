d057c72b30de1eef156ba2102486d0294e488a45
Aaryan-Daw <114655216+Aaryan10000@users.noreply.github.com>
2025-10-30 05:05:05 +0530

    Change output path for similarity matrices

--- downloadModel.py
+++ downloadModel.py
@@ -11,7 +11,7 @@ def download_from_drive(file_id, output_path):
 
 if __name__ == "__main__":
     file_id = "1lcOZ5tcGYr9UMf9Eg0pYJpFkJxhXLwFJ"
-    output_path = os.path.join("recommender_Model", "similarity_matrices.pkl")
+    output_path = os.path.join("models", "similarity_matrices.pkl")
 
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     download_from_drive(file_id, output_path)
